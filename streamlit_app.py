542b501681ef64221ea92d0758d6ae48bd4953a7
Pepper-Bite <jsoto2005@unm.edu>
2025-11-17 00:11:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
-import streamlit as st
-
-st.title("Housing Group 8 Project")
-st.write("App is loading...")
-
-# Import your modules
-import python_files.Annual_Macroeconomic_Factors as macro
-import python_files.data_cleaning as dc
-
-# Example of running a function (you will revise later)
-# st.write(dc.clean_population_data())
+import python_files.Annual_Macroeconomic_Factors as MacroF
+import python_files.Housing as Housing
+import python_files.Population_report as Population
+import python_files.poverty_report as Poverty
+import python_files.Unemployment as Unemployment
